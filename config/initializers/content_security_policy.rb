2776bba7e03fc43e92719aac84f24089003fdae5
sagacic-tim <tmichel@sagacicweb.com>
2026-03-25 20:03:06 -0700

    Make dev SSL protocol driven by env vars, not hardcoded https

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,11 +1,16 @@
 
 Rails.application.configure do
+  api_protocol = ENV.fetch("APP_PROTOCOL", "https")
+  api_host     = ENV.fetch("APP_HOST", "localhost")
+  api_port     = ENV.fetch("APP_PORT", "3001")
+  api_origin   = "#{api_protocol}://#{api_host}:#{api_port}"
+
   config.content_security_policy do |p|
     p.default_src :self
     p.script_src  :self
     p.style_src   :self
     p.img_src     :self, :data
-    p.connect_src :self, 'https://localhost:3001' # API
+    p.connect_src :self, api_origin
     p.frame_ancestors :none
     p.base_uri :self
   end
